aa8eb45698fc9a2741904c0a7bf18b2d3db657bb
KIEN <kienap12f@gmail.com>
2024-03-24 01:24:47 +0700

    update-css

--- dev-job/src/app/app.component.ts
+++ dev-job/src/app/app.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 // import * as data from '../assets/data.json'
 import { NgFor, NgStyle } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from '../header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HttpClientModule, NgFor , HeaderComponent, NgStyle ],
+  imports: [HttpClientModule,FormsModule , NgFor , HeaderComponent, NgStyle ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
+
 export class AppComponent {
   title = 'dev-job';
-
+  isDayMode: boolean = true;
 
   jobs: any[]= [];
 
@@ -25,9 +27,5 @@ export class AppComponent {
         this.jobs = data;  
       });
   }
-
-
-  
-
 
 }
