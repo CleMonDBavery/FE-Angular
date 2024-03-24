ca1d81382ee77734b0180732dd572a146126a5f5
KIEN <kienap12f@gmail.com>
2024-03-24 19:16:01 +0700

    addcomponents

--- dev-job/src/app/app.component.ts
+++ dev-job/src/app/app.component.ts
@@ -1,21 +1,23 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, Input, NgModule, OnInit } from '@angular/core';
 // import * as data from '../assets/data.json'
-import { NgFor, NgStyle } from '@angular/common';
+import { NgClass, NgFor, NgStyle } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from '../header/header.component';
 import { FormsModule } from '@angular/forms';
+import { CardComponent } from '../card/card.component';
+import { ButtonComponent } from '../button/button.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HttpClientModule,FormsModule , NgFor , HeaderComponent, NgStyle ],
+  imports: [HttpClientModule,FormsModule , NgClass , NgFor , CardComponent , HeaderComponent, NgStyle, ButtonComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'dev-job';
-  isDayMode: boolean = true;
 
   jobs: any[]= [];
 
@@ -26,6 +28,13 @@ export class AppComponent {
       .subscribe(data => {
         this.jobs = data;  
       });
+  }
+
+  colorMode = 'lightMode';
+
+  toggleMode() {
+    console.log ('change thanh cong')
+    this.colorMode = this.colorMode === 'lightMode' ? 'nightMode' : 'lightMode';
   }
 
 }
--- dev-job/src/header/header.component.ts
+++ dev-job/src/header/header.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Output } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+ 
+  @Output() toggleMode = new EventEmitter<void>();
 
+  constructor() {}
+
+  onToggleMode() {
+    this.toggleMode.emit();
+  }
 }
